3bf9dae87ba981efef1e0443b1e6d4d6d1d93d0f
richardwellerson <richardwellerson@gmail.com>
2020-02-13 21:38:31 -0300

    Upando path

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={MovieList} />
-        <Route path="/movies/new" component={NewMovie} />
-        <Route exact path="/movies/:id" component={MovieDetails} />
-        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/movie-card-library-crud" component={MovieList} />
+        <Route exact path="/movie-card-library-crud/movies/new" component={NewMovie} />
+        <Route exact path="/movie-card-library-crud/movies/:id" component={MovieDetails} />
+        <Route exact path="/movie-card-library-crud/movies/:id/edit" component={EditMovie} />
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
