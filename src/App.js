1b6d21268983a0bb3c6204011c60e91b7c338f4b
richardwellerson <richardwellerson@gmail.com>
2020-02-13 21:29:49 -0300

    testando com a barra

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="" component={MovieList} />
+        <Route exact path="/" component={MovieList} />
         <Route path="/movies/new" component={NewMovie} />
         <Route exact path="/movies/:id" component={MovieDetails} />
         <Route path="/movies/:id/edit" component={EditMovie} />
